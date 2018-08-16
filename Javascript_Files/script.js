2cf0024e4ff77cddea7697984f92439f06d174a8
Eduardo Pena <p3eduardo0@gmail.com>
2018-08-16 11:34:18 -0400

    third commit

--- Javascript_Files/script.js
+++ Javascript_Files/script.js
@@ -2,73 +2,100 @@ const selectedTiles = []; // selected tiles array
 let tilesIds = [];  // Tile ids array
 const patternTiles = []; // Pattern array
 const lostTiles = []; // Tiles that are incorrect and not in the pattern, tiles the users has not yet selected
+let canPlay = false; // Boundaries for the pattern to be completed
 
 let playingTile = document.querySelectorAll(".playingtile");
 
-//changes tile colorOnclick
-playingTile.forEach((e)=>{
-	e.addEventListener("click",colorChange);
-});
 
-//changes colorOnClick to red on all tiles if you click on the wrong tile
-function colorChange(e){
-
-
-    if (!e.target.classList.contains('colorOnClick'))
+//changes colors of the tiles for it's specific class
+async function colorChange(e){
+    if(canPlay)
     {
-     lost();
-    }
-    else
-    {
-        document.getElementById(e.target.id).classList.remove("fadeColor");
-				document.getElementById(e.target.id).classList.add("colorOnClick");
-        if(patternTiles.indexOf(e.target.id)!== -1)
+        console.log("E target value: ", e.target.id)
+        console.log("patternTiles inside colorChange: ",patternTiles)
+        console.log("classList: ",e.target.classList)
+        //console.log("classList: ",e.target.classList == patternTiles)
+        if (patternTiles.indexOf(e.target.id) == -1 && !e.target.classList.contains('chosen'))//!e.target.classList.contains('colorOnClick'))
         {
-            patternTiles.splice(patternTiles.indexOf(e.target.id),1);
-        };
-        if(patternTiles.length === 0)
+        lost();
+        }
+        else
         {
-            win();
+            document.getElementById(e.target.id).classList.add('chosen');
+            console.log(e.target.id);
+            if(patternTiles.indexOf(e.target.id)!== -1)
+            {
+                patternTiles.splice(patternTiles.indexOf(e.target.id),1);
+            }
+            console.log(patternTiles);
+
+            if(patternTiles.length === 0)
+            {
+                win();
+            }
         }
     }
 }
+
 // Random generate Function for pattern and wrong tiles turning red
 function timer(ms)
 {
     return new Promise(res => setTimeout(res, ms));
 }
+
  async function randGene()
  {
     for(let q = 1; q<=25; q++)
     {
         tilesIds.push('playTile' + q);
     }
+
     let patternLength = Math.floor(Math.random()*3)+7;
 
     for(let c = 0, item = 0,q = 0; c < patternLength; c++)
     {
         q = Math.floor(Math.random()*tilesIds.length);
         items = tilesIds[q];
+        console.log(items);
         tilesIds.splice(q,1);
         patternTiles.push(items);
+        console.log(c);
         document.getElementById(items).classList.add('colorOnClick');
-        await timer(2400)
-				fading()
+        await timer(2400);
+        fading()
+        console.log(patternTiles);
     }
+    console.log("patternTiles inside randGene: ",patternTiles);
+    canPlay = true;
  }
+
  // Timer for the game to start
- setTimeout(randGene, 5000);
-// Timer for blocks to disappear right after appearing.
+ //setTimeout(randGene, 5000);
+
+async function startGame()
+{
+   await timer(5000);
+    randGene();
+    //changes tile colorOnclick
+    playingTile.forEach((e)=>{
+        e.addEventListener("click",colorChange);
+    });
+}
+
+// Timer for blocks to disappear right after the other.
 async function fading()
 {
+    console.log("start fading function");
     for(let i = 0; i <= patternTiles.length; i++)
     {
         item = patternTiles[i];
-        document.getElementById(items).classList.add("fadeColor");
+        document.getElementById(items).classList.toggle("colorOnClick",false);
+        //document.getElementById(items).classList.add("fadeColor");
         await timer(2400);
     }
 }
-//When you click on the wrong tile thats not in the pattern randomly generated
+
+//When you click on the wrong tile thats not in the randomly generated pattern
 // it turns red
 async function lost()
 {
@@ -77,16 +104,27 @@ async function lost()
     {
         tilesIds.push('playTile' + i);
     }
+
+    for(let q = 0; q<=24; q++)
+    {
+        console.log(tilesIds[q]);
+    }
+
     let patternLength = 25;
+
     for(let c = 0, item = 0,i = 0; c < patternLength; c++)
     {
         item = tilesIds[c];
         lostTiles.push(item);
+        console.log(c);
         document.getElementById(item).classList.add('failOnClick');
-        await timer(20);
+        await timer(0);
     }
-
+    console.log("patternTiles inside randGene: ",patternTiles)
 }
+
+//When the array of pattern tiles is empty the win function is called
+//every tile turns green
 async function win()
 {
     tilesIds = [];
@@ -94,28 +132,21 @@ async function win()
     {
         tilesIds.push('playTile' + i);
     }
+
+    for(let q = 0; q<=24; q++)
+    {
+        console.log(tilesIds[q]);
+    }
+
     let patternLength = 25;
 
     for(let c = 0, item = 0,i = 0; c < patternLength; c++)
     {
         item = tilesIds[c];
         lostTiles.push(item);
+        console.log(c);
         document.getElementById(item).classList.add('rightOnClick');
-        await timer(20);
+        await timer(10);
     }
-  }
-
-
-//Challenges
-
-	//1.RandGene: It was hard since we didn't know what an async function was and how we could use it randomly generate our blocks 1 by one
-	//since the for loop wasn't working.
-
-	//2.The lost() function which took us around 5-6 hours to find a fix.
-	// in this problem we had to create a function that checked if we clicked the correct block, and if it wasn't the correct one
-	//it would populate all the blocks with a class but for some reason some of the blocks weren't being populated with that class which left
-	//some blocks blank.
-
-//Accomplishments
-	//We got most of it working except for a glitch which turns a block back to blank.
-	//We managed to implement webapp2 into our code.
+    console.log("patternTiles inside randGene: ",patternTiles)
+}
